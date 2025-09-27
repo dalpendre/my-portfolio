8733c8e95e1d20a7f506a84bb435c0ba34741059
Diogo Alpendre <dalpendre1999@gmail.com>
2025-09-27 22:46:11 +0100

    Fix: imgs location

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -49,7 +49,7 @@ export default function RootLayout({ children }) {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image aria-hidden src="/github-color.svg" alt="GitHub" width={16} height={16} />
+              <Image aria-hidden src="/img/github-color.svg" alt="GitHub" width={16} height={16} />
               GitHub
             </a>
             <a
@@ -58,7 +58,7 @@ export default function RootLayout({ children }) {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image aria-hidden src="/linkedin-color.svg" alt="LinkedIn" width={16} height={16} />
+              <Image aria-hidden src="/img/linkedin-color.svg" alt="LinkedIn" width={16} height={16} />
               Linkedin
             </a>
           </footer>
